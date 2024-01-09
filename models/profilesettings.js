ed3034f0b6fee09fa28611b6f2324c13986f2742
Bogdan Putineanu <94795061+197Bogdan@users.noreply.github.com>
2024-01-10 00:31:36 +0200

    fixed user - profileSettings association

--- models/profilesettings.js
+++ models/profilesettings.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.ProfileSettings.hasOne(models.User);
+      models.ProfileSettings.belongsTo(models.User);
     }
   }
   ProfileSettings.init(
